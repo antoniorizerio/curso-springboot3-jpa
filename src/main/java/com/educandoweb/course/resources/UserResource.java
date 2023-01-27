8bc53598dea0e11ef171c83572dee57f3b23d75d
Antonio Rizerio <antonio.rizerio@gmail.com>
2023-01-27 15:27:46 -0300

    Criacao do UserService, alteração do UserRepository, criacao do metodo findById(Long Id)

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,10 +1,15 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 /**
  * Representa o Controlador Rest referente a entidade User.
@@ -18,13 +23,22 @@ import com.educandoweb.course.entities.User;
 //	**  Nome do meu recurso '/users' //
 @RequestMapping(value = "/users") 
 public class UserResource {
+
+	@Autowired // Injeção de Dependência //
+	private UserService userService;
 	
 	// Crio um endPoint para acessar usuários //
 	// Responde a requisição tipo GET do http //
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> listUser = userService.findAll();
+		return ResponseEntity.ok().body(listUser);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
