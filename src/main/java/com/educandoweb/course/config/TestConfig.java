c5e59108b7607ccc0068518f0c04c77d193b3efa
Antonio Rizerio <antonio.rizerio@gmail.com>
2023-02-17 13:15:02 -0300

    Criacao Entidade Category - Repository, Resource e Service

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -9,9 +9,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatusEnum;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -31,6 +33,10 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 
 	// Tudo que estiver dentro desse método será executado quando a aplicação
 	// for iniciada //
@@ -48,6 +54,12 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatusEnum.WAITING_PAYMENT, u2);
 		
 		orderRepository.saveAll( Arrays.asList(o1,o2,o3) );
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 	}
 
--- src/main/java/com/educandoweb/course/services/CategoryService.java
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -5,22 +5,25 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 //Registrando como um componente do Spring ou @Component //
 @Service // Tem uma semântica melhor, indicando que é um camada de negócio que estende @Component //
-public class UserService {
+public class CategoryService {
 
 	@Autowired // Injeção de dependência //
-	private UserRepository repository;
+	private CategoryRepository repository;
 	
-	public List<User> findAll() {
+	public List<Category> findAll() {
 		return repository.findAll();
 	}
 	
-	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id);
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
 		return obj.orElse(null);
 	}
 }
