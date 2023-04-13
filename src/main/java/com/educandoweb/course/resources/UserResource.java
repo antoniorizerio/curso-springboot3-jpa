10bb6b1c0bab9a22bed4f0e30d99bca4cb60606c
Antonio Rizerio <antonio.rizerio@gmail.com>
2023-04-13 20:53:45 -0300

    Opração de Delete da entidade User e Controller UserResource.

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,4 +71,12 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 
+	// endpoint de deleção //
+	// @PathVariable: um parâmetro será passado na URL //
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		userService.delete(id);
+		// noContent(): Pois na responsta não vai nada, retorna um 204.
+		return ResponseEntity.noContent().build();
+	}
 }
